a0bc90bf208bbbe3d3cc01208db3c866b5413e86
David <chdavid0422@gmail.com>
2026-09-17 14:04:22 +0800

    Go back

--- doom-v2/software/ps2_keys.h
+++
@@ -1,6 +0,0 @@
-#ifndef DOOM_PS2_KEYS_H
-#define DOOM_PS2_KEYS_H
-#include <stdint.h>
-typedef struct {uint8_t extended,released,pause_skip;} Ps2Parser;
-int ps2_key(Ps2Parser *parser,uint8_t byte,int *pressed,uint8_t *key);
-#endif
--- doom-v2/tools/check_project_open.tcl
+++
@@ -1,4 +0,0 @@
-package require ::quartus::project
-project_open doom_de2_115
-puts "DOOM_PROJECT_OPEN_OK [get_current_project]"
-project_close
